daf9a1f92c56d4060f6f6bc67829a13c75b72bfb
Mac <mohammed.ali.chherawalla@gmail.com>
2022-03-04 16:15:36 +0530

    add support to apply all styles

--- figma-react-bridge/app/components/Button/index.js
+++ figma-react-bridge/app/components/Button/index.js
@@ -5,13 +5,31 @@
  */
 
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import globalStyleConfig from '../../../../tokens.json';
 
+const mapper = {
+  sizing: { type: 'number', values: ['height', 'width'] }
+};
 export function Button({ children }) {
-  const style = globalStyleConfig.global.Primary;
+  const styles = {};
+  Object.keys(globalStyleConfig.global).map((key) => {
+    const map = mapper[globalStyleConfig.global[key].type];
+    if (map) {
+      map.values.forEach((type) => {
+        if (map.type === 'number') {
+          styles[type] = parseInt(globalStyleConfig.global[key].value, 10);
+        } else {
+          styles[type] = globalStyleConfig.global[key].value;
+        }
+      });
+    } else {
+      styles[globalStyleConfig.global[key].type.toLowerCase()] = globalStyleConfig.global[key].value;
+    }
+  });
+  console.log({ styles });
   return (
-    <button data-testid="button" style={{ [style.type]: style['value'] }}>
+    <button data-testid="button" style={styles}>
       {children}
     </button>
   );
