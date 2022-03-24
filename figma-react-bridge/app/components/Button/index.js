dda8d2b45529b5eec3f1b9c6767730696ac36e65
Mac <mohammed.ali.chherawalla@gmail.com>
2022-03-24 16:30:17 +0530

    add support for units

--- figma-react-bridge/app/components/Button/index.js
+++ figma-react-bridge/app/components/Button/index.js
@@ -6,25 +6,49 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
 import globalStyleConfig from '../../../../tokens.json';
 
 const mapper = {
-  sizing: { type: 'number', values: ['height', 'width'] }
+  sizing: { type: 'number', values: ['height', 'width'], unit: 'px' },
+  spacing: { type: 'number', values: ['padding'], unit: 'px' },
+  borderWidth: {
+    unit: 'px'
+  },
+  borderRadius: {
+    unit: 'px'
+  }
 };
 export function Button({ children }) {
   const styles = {};
   Object.keys(globalStyleConfig.global).map((key) => {
     const map = mapper[globalStyleConfig.global[key].type];
-    if (map) {
-      map.values.forEach((type) => {
+    if (map?.values) {
+      const constraint = map.values.find((v) => v.toUpperCase() === key.toUpperCase());
+      if (constraint) {
         if (map.type === 'number') {
-          styles[type] = parseInt(globalStyleConfig.global[key].value, 10);
+          styles[key.toLowerCase()] = parseInt(globalStyleConfig.global[key].value, 10) + map.unit;
         } else {
-          styles[type] = globalStyleConfig.global[key].value;
+          styles[type.toLowerCase()] = globalStyleConfig.global[key].value;
         }
-      });
+      }
     } else {
-      styles[globalStyleConfig.global[key].type.toLowerCase()] = globalStyleConfig.global[key].value;
+      let value = globalStyleConfig.global[key].value;
+      let isNum = false;
+      try {
+        const num = parseInt(value, 10);
+        if (Number.isInteger(num)) {
+          isNum = true;
+        }
+      } catch (err) {}
+      if (isNum) {
+        console.log(Number.isNaN(value));
+        console.log({ value, a: globalStyleConfig.global[key] });
+        value = `${value}` + mapper[globalStyleConfig.global[key].type].unit;
+      }
+      styles[globalStyleConfig.global[key].type.toLowerCase()] = value;
+      styles[kebabCase(globalStyleConfig.global[key].type)] = value;
+      styles[globalStyleConfig.global[key].type] = value;
     }
   });
   console.log({ styles });
